2bfd67526f56bf5625521cc6f07389b7099e0b52
Igbanesi michael <eshimi090@gmail.com>
2025-12-06 20:56:46 +0100

    Particle header

---
+++ First2dGame/Particle.cpp
@@ -0,0 +1,7 @@
+#include "Particle.h"
+
+Particle::Particle(size_t id) : Entity("particle", id)
+{
+	/*lifespan.remaining = 0.5f;
+	collider.enabled = false;*/
+}
--- First2dGame/Particle.h
+++ First2dGame/Particle.h
@@ -1,2 +1,9 @@
 #pragma once
 //visual effects for animations
+#include "Entity.h"
+
+class Particle : public Entity
+{
+public:
+	Particle(size_t id)
+};
